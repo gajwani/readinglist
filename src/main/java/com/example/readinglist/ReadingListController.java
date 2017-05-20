dc0bb64c68ffd07561e2419cdfb6674f5c72dd3d
Winnie Kwon <lkwon@pivotal.io>
2017-05-19 17:05:08 -0700

    add path parameter to the controller

--- src/main/java/com/example/readinglist/ReadingListController.java
+++ src/main/java/com/example/readinglist/ReadingListController.java
@@ -3,16 +3,15 @@ package com.example.readinglist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/readingList")
+@RequestMapping("/readingList/{reader}")
 public class ReadingListController {
-
-    private static final String reader = "craig";
 
     private ReadingListRepository readingListRepository;
 
@@ -22,7 +21,7 @@ public class ReadingListController {
     }
 
     @RequestMapping(method=RequestMethod.GET)
-    public String readersBooks(Model model) {
+    public String readersBooks(@PathVariable("reader") String reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null)
             model.addAttribute("books", readingList);
@@ -30,9 +29,9 @@ public class ReadingListController {
     }
 
     @RequestMapping(method=RequestMethod.POST)
-    public String addToReadingList(Book book) {
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
-        return "redirect:/readingList";
+        return "redirect:/readingList/{reader}";
     }
 }
