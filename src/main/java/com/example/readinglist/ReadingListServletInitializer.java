7ce07580b2e48dc7673ead71df645160fdd950cb
Winnie <heladolk@yahoo.com>
2017-05-25 11:50:47 -0700

    WIP: prepare the app for deployment and add production database
    - add war plugin to build.gradle
    - add ServletInitializer for war file
    - add database migration scheme to be used in production
    	- flyway
    	- liquibase: this is the one mainly used
    
    - status: setting up the database worked
    - TODO: in production environment, log in doesn't work due to database issue of connecting book and reader.

---
+++ src/main/java/com/example/readinglist/ReadingListServletInitializer.java
@@ -0,0 +1,12 @@
+package com.example.readinglist;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ReadingListServletInitializer extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ReadingListApplication.class);
+  }
+}
